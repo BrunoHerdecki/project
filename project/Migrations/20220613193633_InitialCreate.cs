d495a69b61da8cb900feb7143b934fbbeae3f295
BrunoHerdecki <bruno.herdecki@microsoft.wsei.edu.pl>
2022-06-13 21:37:47 +0200

    Add project files.

---
+++ project/Migrations/20220613193633_InitialCreate.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace project.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "courses",
+                columns: table => new
+                {
+                    CoursesId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CourseName = table.Column<string>(type: "TEXT", nullable: true),
+                    TeacherId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_courses", x => x.CoursesId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "participants",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: true),
+                    Password = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "teachers",
+                columns: table => new
+                {
+                    TeacherId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: true),
+                    Password = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teachers", x => x.TeacherId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "courses");
+
+            migrationBuilder.DropTable(
+                name: "participants");
+
+            migrationBuilder.DropTable(
+                name: "students");
+
+            migrationBuilder.DropTable(
+                name: "teachers");
+        }
+    }
+}
---
+++ project/Model.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace project
+{
+    public class Model : DbContext
+    {
+        public DbSet<Student> students { get; set; }
+        public DbSet<Teacher> teachers { get; set; }
+        public DbSet<Courses> courses { get; set; }
+        public DbSet<Participants> participants { get; set; }
+
+        public string DbPath { get; }
+
+        public Model()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "project.db");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Participants>(
+                    en =>
+                    {
+                        en.HasNoKey();
+                    });
+        }
+    }
+
+
+    public class Teacher
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int TeacherId { get; set; }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Courses
+    {
+        public int CoursesId { get; set; }
+        public string CourseName { get; set; }
+        public int TeacherId { get; set; }
+    }
+
+    public class Participants
+    {
+        public int CourseId { get; set; }
+        public int UserId { get; set; }
+    }
+}
+
